e25a241f06cdb3f51cbb319ee0cf0dacb7acffe4
Die <dlibonati@leafnoise.io>
2025-01-17 10:32:36 -0300

    feat: better css config and tests config

--- src/helpers/getRandomIndex.test.ts
+++ src/helpers/getRandomIndex.test.ts
@@ -1,11 +1,15 @@
 import { getRandomIndex } from "./getRandomIndex";
 
-const arr = ["123", "32", "12"];
+describe("getRandomIndex.ts", () => {
+  describe("General Tests.", () => {
+    const arr = ["123", "32", "12"];
 
-test("It should return a random number within the possible range of the input array.", () => {
-  const minIndex = 0;
-  const maxIndex = arr.length - 1;
-  const randomNumber = getRandomIndex<string>(arr);
+    test("It should return a random number within the possible range of the input array.", () => {
+      const minIndex = 0;
+      const maxIndex = arr.length - 1;
+      const randomNumber = getRandomIndex<string>(arr);
 
-  expect(randomNumber >= minIndex && randomNumber <= maxIndex).toBeTruthy();
+      expect(randomNumber >= minIndex && randomNumber <= maxIndex).toBeTruthy();
+    });
+  });
 });
